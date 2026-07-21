105970f0aae23a4363106fa91e16a6d3ea822653
DeaNingrum <deaningrumputri0@gmail.com>
2026-07-21 11:17:22 +0700

    Refactor success.php to include footer and remove section

--- app/Views/pengaduan/success.php
+++ app/Views/pengaduan/success.php
@@ -1,6 +1,4 @@
-<?= $this->extend('layout/main') ?>
-
-<?= $this->section('content') ?>
+<?= $this->include('layout/header') ?>
 
 <style>
 .success-wrapper {
@@ -40,4 +38,4 @@
     </div>
 </div>
 
-<?= $this->endSection() ?>
+<?= $this->include('layout/footer') ?>
