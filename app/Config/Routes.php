6b8481ef7089d556da602aafebee6a8371e09b25
dwiratna1 <dwiratnas138@gmail.com>
2026-07-29 08:26:05 +0700

    route tentang kami

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -58,6 +58,7 @@ $routes->get('korsda/peta/(:num)', 'Korsda::peta/$1');
 $routes->get('korsda/kegiatan/(:num)', 'Korsda::kegiatan/$1');
 $routes->get('korsda/detail_kegiatan/(:num)', 'Korsda::detailKegiatan/$1');
 
+$routes->get('tentang-kami', 'TentangKami::index');
 /*
 |--------------------------------------------------------------------------
 | ADMIN
